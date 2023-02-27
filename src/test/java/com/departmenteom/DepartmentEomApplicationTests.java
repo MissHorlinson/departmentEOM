11a90417af947a3a3c8a92094c96f34c889c0acb
MissHorlinson <56289427+MissHorlinson@users.noreply.github.com>
2023-02-27 14:33:22 +0200

    tests

---
+++ src/test/java/com/departmenteom/DepartmentEomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.departmenteom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentEomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
